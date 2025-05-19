394e3bfbebbccdf897fa7deacd38c2989d9a487c
Dante-Sal2 <f1apiproyecto.s1@gmail.com>
2025-05-19 01:03:43 +0000

    fix: :bug: Refining redirection solution (implementing a conditional statement)

--- js/login/login.js
+++ js/login/login.js
@@ -2,5 +2,9 @@ const login_form = document.querySelector('.login-container__form');
 
 login_form.addEventListener('submit', e => {
     e.preventDefault();
-    location.href = `${window.location.href}pages/home.html`;
+    if ('index.html' in window.location.href) {
+        location.href = `${window.location.href.replace('index.html', '')}pages/home.html`;
+    } else {
+        location.href = `${window.location.href}pages/home.html`;
+    };
 });
