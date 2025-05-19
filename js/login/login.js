822150dabb0c3d6f766d1507dbf31e8bdc6d73e6
Dante-Sal2 <f1apiproyecto.s1@gmail.com>
2025-05-19 00:51:56 +0000

    fix: :alembic: Correcting redirection error (link alternative)

--- js/login/login.js
+++ js/login/login.js
@@ -2,5 +2,5 @@ const login_form = document.querySelector('.login-container__form');
 
 login_form.addEventListener('submit', e => {
     e.preventDefault();
-    location.href = '/pages/home.html';
+    location.href = `${window.location.href}/pages/home.html`;
 });
